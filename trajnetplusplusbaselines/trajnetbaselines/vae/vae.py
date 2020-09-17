057e5f2824cea5095fa871a75cdceca12c2280cf
Arturjesslen <artur.jesslen@epfl.ch>
2020-09-17 15:23:00 +0200

    first step vae in progress

--- trajnetplusplusbaselines/trajnetbaselines/vae/vae.py
+++ trajnetplusplusbaselines/trajnetbaselines/vae/vae.py
@@ -1,30 +1,249 @@
 """ VAE class definition """
+import itertools
 
 import torch
 
+from ..lstm.modules import InputEmbedding, Hidden2Normal
+
+NAN = float('nan')
+
 class VAE(torch.nn.Module):
-    def __init__(self, embedding_dim = 64, hidden_dim = 128):
+    def __init__(self, embedding_dim=64, hidden_dim=128, pool=None, pool_to_input=True, goal_dim=None, goal_flag=False):
         """ Initialize the VAE forecasting model
 
         Attributes
         ----------
         embedding_dim : Embedding dimension of location coordinates
         hidden_dim : Dimension of hidden state of LSTM
+        pool : interaction module
+        pool_to_input : Bool
+            if True, the interaction vector is concatenated to the input embedding of LSTM [preferred]
+            if False, the interaction vector is added to the LSTM hidden-state
+        goal_dim : Embedding dimension of the unit vector pointing towards the goal
+        goal_flag: Bool
+            if True, the embedded goal vector is concatenated to the input embedding of LSTM 
         """
         super(VAE, self).__init__()
 
         self.embedding_dim = embedding_dim
         self.hidden_dim = hidden_dim
+        self.pool = pool
+        self.pool_to_input = pool_to_input
+
+        ## Location
+        scale = 4.0
+        self.input_embedding = InputEmbedding(2, self.embedding_dim, scale)
+
+        ## Goal
+        self.goal_flag = goal_flag
+        self.goal_dim = goal_dim or embedding_dim
+        self.goal_embedding = InputEmbedding(2, self.goal_dim, scale)
+        goal_rep_dim = self.goal_dim if self.goal_flag else 0
+
+        ## Pooling
+        pooling_dim = 0
+        if pool is not None and self.pool_to_input:
+            pooling_dim = self.pool.out_dim 
 
         ## LSTMs
-        self.encoder1 = torch.nn.LSTMCell(self.embedding_dim, self.hidden_dim)
-        self.encoder2 = torch.nn.LSTMCell(self.embedding_dim, self.hidden_dim)
-        self.decoder = torch.nn.LSTMCell(2*self.hidden_dim, self.embedding_dim)
+        self.encoder1 = torch.nn.LSTMCell(self.embedding_dim + goal_rep_dim + pooling_dim, self.hidden_dim)
+        self.encoder2 = torch.nn.LSTMCell(self.embedding_dim + goal_rep_dim + pooling_dim, self.hidden_dim)
+        self.decoder = torch.nn.LSTMCell(2*(self.embedding_dim + goal_rep_dim + pooling_dim), self.embedding_dim)
 
-    def forward(self):
-        """ Forward path for VAE forecasting model
+        # Predict the parameters of a multivariate normal:
+        # mu_vel_x, mu_vel_y, sigma_vel_x, sigma_vel_y, rho
+        self.hidden2normal = Hidden2Normal(self.hidden_dim)
 
-        Attributes
+
+    def step(self, lstm, hidden_cell_state, obs1, obs2, goals, batch_split):
+        """Do one step of prediction: two inputs to one normal prediction.
+        
+        Parameters
         ----------
+        lstm: torch nn module [Encoder / Decoder]
+            The module responsible for prediction
+        hidden_cell_state : tuple (hidden_state, cell_state)
+            Current hidden_cell_state of the pedestrians
+        obs1 : Tensor [num_tracks, 2]
+            Previous x-y positions of the pedestrians
+        obs2 : Tensor [num_tracks, 2]
+            Current x-y positions of the pedestrians
+        goals : Tensor [num_tracks, 2]
+            Goal coordinates of the pedestrians
+        
+        Returns
+        -------
+        hidden_cell_state : tuple (hidden_state, cell_state)
+            Updated hidden_cell_state of the pedestrians
+        normals : Tensor [num_tracks, 5]
+            Parameters of a multivariate normal of the predicted position 
+            with respect to the current position
         """
-        raise NotImplementedError
+        num_tracks = len(obs2)
+        # mask for pedestrians absent from scene (partial trajectories)
+        # consider only the hidden states of pedestrains present in scene
+        track_mask = (torch.isnan(obs1[:, 0]) + torch.isnan(obs2[:, 0])) == 0
+
+        ## Masked Hidden Cell State
+        hidden_cell_stacked = [
+            torch.stack([h for m, h in zip(track_mask, hidden_cell_state[0]) if m], dim=0),
+            torch.stack([c for m, c in zip(track_mask, hidden_cell_state[1]) if m], dim=0),
+        ]
+
+        ## Mask current velocity & embed
+        curr_velocity = obs2 - obs1
+        curr_velocity = curr_velocity[track_mask]
+        input_emb = self.input_embedding(curr_velocity)
+
+        ## Mask Goal direction & embed
+        if self.goal_flag:
+            ## Get relative direction to goals (wrt current position)
+            norm_factors = (torch.norm(obs2 - goals, dim=1))
+            goal_direction = (obs2 - goals) / norm_factors.unsqueeze(1)
+            goal_direction[norm_factors == 0] = torch.Tensor([0., 0.], device=obs1.device)
+            goal_direction = goal_direction[track_mask]
+            goal_emb = self.goal_embedding(goal_direction)
+            input_emb = torch.cat([input_emb, goal_emb], dim=1)
+
+        ## Mask & Pool per scene
+        if self.pool is not None:
+            hidden_states_to_pool = torch.stack(hidden_cell_state[0]).clone() # detach?
+            batch_pool = []
+            ## Iterate over scenes
+            for (start, end) in zip(batch_split[:-1], batch_split[1:]):
+                ## Mask for the scene
+                scene_track_mask = track_mask[start:end]
+                ## Get observations and hidden-state for the scene
+                prev_position = obs1[start:end][scene_track_mask]
+                curr_position = obs2[start:end][scene_track_mask]
+                curr_hidden_state = hidden_states_to_pool[start:end][scene_track_mask]
+
+                # LSTM-Based Interaction Encoders. Provide track_mask to the interaction encoder LSTMs
+                if self.pool.__class__.__name__ in {'NN_LSTM', 'TrajectronPooling', 'SAttention_fast'}:
+                    ## Everyone absent by default
+                    interaction_track_mask = torch.zeros(num_tracks, device=obs1.device).bool()
+                    ## Only those visible in current scene are present
+                    interaction_track_mask[start:end] = track_mask[start:end]
+                    self.pool.track_mask = interaction_track_mask
+
+                pool_sample = self.pool(curr_hidden_state, prev_position, curr_position)
+                batch_pool.append(pool_sample)
+
+            pooled = torch.cat(batch_pool)
+            if self.pool_to_input:
+                input_emb = torch.cat([input_emb, pooled], dim=1)
+            else:
+                hidden_cell_stacked[0] += pooled
+
+        # LSTM step
+        hidden_cell_stacked = lstm(input_emb, hidden_cell_stacked)
+        normal_masked = self.hidden2normal(hidden_cell_stacked[0])
+
+        # unmask [Update hidden-states and next velocities of pedestrians]
+        normal = torch.full((track_mask.size(0), 5), NAN, device=obs1.device)
+        mask_index = [i for i, m in enumerate(track_mask) if m]
+        for i, h, c, n in zip(mask_index,
+                              hidden_cell_stacked[0],
+                              hidden_cell_stacked[1],
+                              normal_masked):
+            hidden_cell_state[0][i] = h
+            hidden_cell_state[1][i] = c
+            normal[i] = n
+
+        return hidden_cell_state, normal
+
+    def forward(self, observed, goals, batch_split, prediction_truth=None, n_predict=None):
+        """Forecast the entire sequence 
+        
+        Parameters
+        ----------
+        observed : Tensor [obs_length, num_tracks, 2]
+            Observed sequences of x-y coordinates of the pedestrians
+        goals : Tensor [num_tracks, 2]
+            Goal coordinates of the pedestrians
+        batch_split : Tensor [batch_size + 1]
+            Tensor defining the split of the batch.
+            Required to identify the tracks of to the same scene        
+        prediction_truth : Tensor [pred_length - 1, num_tracks, 2]
+            Prediction sequences of x-y coordinates of the pedestrians
+            Helps in teacher forcing wrt neighbours positions during training
+        n_predict: Int
+            Length of sequence to be predicted during test time
+
+        Returns
+        -------
+        rel_pred_scene : Tensor [pred_length, num_tracks, 5]
+            Predicted velocities of pedestrians as multivariate normal
+            i.e. positions relative to previous positions
+        pred_scene : Tensor [pred_length, num_tracks, 2]
+            Predicted positions of pedestrians i.e. absolute positions
+        """
+        assert ((prediction_truth is None) + (n_predict is None)) == 1
+        if n_predict is not None:
+            # -1 because one prediction is done by the encoder already
+            prediction_truth = [None for _ in range(n_predict - 1)]
+
+        # initialize: Because of tracks with different lengths and the masked
+        # update, the hidden state for every LSTM needs to be a separate object
+        # in the backprop graph. Therefore: list of hidden states instead of
+        # a single higher rank Tensor.
+        num_tracks = observed.size(1)
+        hidden_cell_state = (
+            [torch.zeros(self.hidden_dim, device=observed.device) for _ in range(num_tracks)],
+            [torch.zeros(self.hidden_dim, device=observed.device) for _ in range(num_tracks)],
+        )
+
+        ## LSTM-Based Interaction Encoders. Initialze Hidden state
+        if self.pool.__class__.__name__ in {'NN_LSTM', 'TrajectronPooling', 'SAttention', 'SAttention_fast'}:
+            self.pool.reset(num_tracks, device=observed.device)
+
+        # list of predictions
+        normals = []  # predicted normal parameters for both phases
+        positions = []  # true (during obs phase) and predicted positions
+
+        if len(observed) == 2:
+            positions = [observed[-1]]
+
+        # encoder1
+        for obs1, obs2 in zip(observed[:-1], observed[1:]):
+            ##LSTM Step
+            hidden_cell_state, normal = self.step(self.encoder1, hidden_cell_state, obs1, obs2, goals, batch_split)
+
+            # concat predictions
+            normals.append(normal)
+            positions.append(obs2 + normal[:, :2])  # no sampling, just mean
+
+        # initialize predictions with last position to form velocity
+        prediction_truth = list(itertools.chain.from_iterable(
+            (observed[-1:], prediction_truth)
+        ))
+
+        # encoder2
+        ## TODO 
+
+        # decoder, predictions
+        for obs1, obs2 in zip(prediction_truth[:-1], prediction_truth[1:]):
+            if obs1 is None:
+                obs1 = positions[-2].detach()  # DETACH!!!
+            else:
+                for primary_id in batch_split[:-1]:
+                    obs1[primary_id] = positions[-2][primary_id].detach()  # DETACH!!!
+            if obs2 is None:
+                obs2 = positions[-1].detach()
+            else:
+                for primary_id in batch_split[:-1]:
+                    obs2[primary_id] = positions[-1][primary_id].detach()  # DETACH!!!
+            hidden_cell_state, normal = self.step(self.decoder, hidden_cell_state, obs1, obs2, goals, batch_split)
+
+            # concat predictions
+            normals.append(normal)
+            positions.append(obs2 + normal[:, :2])  # no sampling, just mean
+
+        # Pred_scene: Tensor [seq_length, num_tracks, 2]
+        #    Absolute positions of all pedestrians
+        # Rel_pred_scene: Tensor [seq_length, num_tracks, 5]
+        #    Velocities of all pedestrians
+        rel_pred_scene = torch.stack(normals, dim=0)
+        pred_scene = torch.stack(positions, dim=0)
+
+        return rel_pred_scene, pred_scene
