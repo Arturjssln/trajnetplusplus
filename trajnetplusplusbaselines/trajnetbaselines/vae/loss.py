86092a0766a86c1a9ab9bd4b2dbeec5ff3fc83ce
Arturjesslen <artur.jesslen@epfl.ch>
2020-09-25 09:57:43 +0200

    kld loss correction (distribution need to be used)

--- trajnetplusplusbaselines/trajnetbaselines/vae/loss.py
+++ trajnetplusplusbaselines/trajnetbaselines/vae/loss.py
@@ -14,17 +14,15 @@ class KLDLoss(torch.nn.Module):
 
     def forward(self, inputs, targets, batch_split):
         ## Extract primary pedestrians
-        # [pred_length, num_tracks, 2] --> [pred_length, batch_size, 2]
+        # [pred_length, num_tracks, 5] --> [pred_length, batch_size, 5]
         targets = targets.transpose(0, 1)
         targets = targets[batch_split[:-1]]
         targets = targets.transpose(0, 1)
-        # [pred_length, num_tracks, 5] --> [pred_length, batch_size, 5]
         inputs = inputs.transpose(0, 1)
         inputs = inputs[batch_split[:-1]]
         inputs = inputs.transpose(0, 1)
 
-        # Only xy components of the multivariate normal distribution
-        return self.loss(inputs[:, :, :2], targets)
+        return self.loss(inputs, targets)
 
 
     
