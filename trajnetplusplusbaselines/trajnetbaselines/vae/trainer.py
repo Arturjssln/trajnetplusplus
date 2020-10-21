1afb37b295d642108bb4743ce41fbb01e6308bbb
Arturjssln <artur.jesslen@epfl.ch>
2020-10-21 15:26:05 +0200

    minor change in gitignore + trainer

--- trajnetplusplusbaselines/trajnetbaselines/vae/trainer.py
+++ trajnetplusplusbaselines/trajnetbaselines/vae/trainer.py
@@ -273,7 +273,7 @@ class Trainer(object):
         for rel_outputs_mode in rel_outputs:
             reconstr_loss += self.criterion(rel_outputs_mode[-self.pred_length:], targets, batch_split) * self.batch_size * self.loss_multiplier / self.num_modes
         # KLD loss
-        kdl_loss = self.kld_loss(inputs=z_distribution) * self.batch_size
+        kld_loss = self.kld_loss(inputs=z_distribution) * self.batch_size
         
         ## Total loss is the sum of the reconstruction loss and the kld loss
         loss = reconstr_loss + self.alpha_kld * kld_loss
@@ -320,8 +320,8 @@ class Trainer(object):
             reconstr_loss = 0
             for rel_outputs_mode in rel_outputs:
                 reconstr_loss += self.criterion(rel_outputs_mode[-self.pred_length:], targets, batch_split) * self.batch_size * self.loss_multiplier / self.num_modes
-            kdl_loss = self.kld_loss(inputs=z_distribution) * self.batch_size * self.loss_multiplier
-            loss = reconstr_loss + self.alpha_kld * kdl_loss
+            kld_loss = self.kld_loss(inputs=z_distribution) * self.batch_size * self.loss_multiplier
+            loss = reconstr_loss + self.alpha_kld * kld_loss
             
             ## groundtruth of neighbours not provided 
             self.model.eval()
