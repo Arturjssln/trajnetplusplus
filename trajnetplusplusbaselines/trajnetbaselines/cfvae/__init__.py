a8011db67a896ad5039c11fe15446c08b3c22b78
Arturjesslen <artur.jesslen@epfl.ch>
2020-12-15 08:51:26 +0100

    minor cleaning

--- trajnetplusplusbaselines/trajnetbaselines/cfvae/__init__.py
+++ trajnetplusplusbaselines/trajnetbaselines/cfvae/__init__.py
@@ -1,4 +1,4 @@
-from .vae import VAE, VAEPredictor
+from .cfvae import CFVAE, VAEPredictor
 from .loss import KLDLoss, PredictionLoss, L2Loss
 from .pooling.gridbased_pooling import GridBasedPooling
 from .pooling.non_gridbased_pooling import NN_Pooling
